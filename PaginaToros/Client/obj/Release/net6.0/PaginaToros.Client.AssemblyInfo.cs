00c74d1a985db8c316f48e0fabcd92392d91024e
juli2kapo <sestojulian@gmail.com>
2024-06-11 15:28:13 -0300

    bunch of changes

--- PaginaToros/Client/obj/Release/net6.0/PaginaToros.Client.AssemblyInfo.cs
+++ PaginaToros/Client/obj/Release/net6.0/PaginaToros.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaginaToros.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f312635d57230e53245425a538a7cb2152c70d87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8692bdf08f7fa26e89c0f3782163c1c07e3953ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaginaToros.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaginaToros.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
