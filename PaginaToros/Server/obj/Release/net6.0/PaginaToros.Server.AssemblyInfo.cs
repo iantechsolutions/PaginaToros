51404cad464dbafa8437a9263e5541fa0ed723e1
CorsaniM <maximocorsani@gmail.com>
2025-02-27 15:55:43 -0300

    subida.

--- PaginaToros/Server/obj/Release/net6.0/PaginaToros.Server.AssemblyInfo.cs
+++ PaginaToros/Server/obj/Release/net6.0/PaginaToros.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaginaToros.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ebad3c594ea72e0cc0fcc3094686b00d1ae08fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+845acc6023f28d27001edf27fa3743b5b5e99180")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaginaToros.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaginaToros.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PaginaToros/Shared/obj/Release/net6.0/PaginaToros.Shared.AssemblyInfo.cs
+++ PaginaToros/Shared/obj/Release/net6.0/PaginaToros.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaginaToros.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdaa99023a241ccd58fa2f36881ad49d1ac3f47c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+845acc6023f28d27001edf27fa3743b5b5e99180")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaginaToros.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaginaToros.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
