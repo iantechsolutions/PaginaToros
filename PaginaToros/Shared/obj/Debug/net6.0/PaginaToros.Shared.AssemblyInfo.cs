27c2efcfb89eaf5732616c4906942b1760c41284
CorsaniM <maximocorsani@gmail.com>
2025-03-19 11:04:10 -0300

    hot

--- PaginaToros/Shared/obj/Debug/net6.0/PaginaToros.Shared.AssemblyInfo.cs
+++ PaginaToros/Shared/obj/Debug/net6.0/PaginaToros.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaginaToros.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0585dc59e69723cc275d9b5c2880ab6659451e3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ed8d8be1fff51ce380bb870b8cdd786b5a92ac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaginaToros.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaginaToros.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
