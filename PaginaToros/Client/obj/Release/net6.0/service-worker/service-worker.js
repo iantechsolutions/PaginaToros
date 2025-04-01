b3e08709565d76f0014228395aa59f7f6f8978a1
CorsaniM <maximocorsani@gmail.com>
2025-04-01 16:21:07 -0300

    4/1

--- PaginaToros/Client/obj/Release/net6.0/service-worker/service-worker.js
+++ PaginaToros/Client/obj/Release/net6.0/service-worker/service-worker.js
@@ -2,4 +2,4 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
-/* Manifest version: c6UuS0KX */
+/* Manifest version: Dmrv+r4n */
--- PaginaToros/Server/obj/Release/net6.0/PaginaToros.Server.AssemblyInfo.cs
+++ PaginaToros/Server/obj/Release/net6.0/PaginaToros.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaginaToros.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ed8d8be1fff51ce380bb870b8cdd786b5a92ac4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6ea4d9f6888abcd5c9fd248d72b0aba66ffcdfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaginaToros.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaginaToros.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
