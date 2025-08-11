478a59f7ccb753243ffa932db205c6a309114799
CorsaniM <maximocorsani@gmail.com>
2025-08-11 11:30:48 -0300

    hotfix

--- PaginaToros/Server/obj/Debug/net6.0/PaginaToros.Server.AssemblyInfo.cs
+++ PaginaToros/Server/obj/Debug/net6.0/PaginaToros.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaginaToros.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e24461631559a34b568a196bdf6b33540bedb25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea59ba76eb7bf95c5436f80d26e1b93be1461ba9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaginaToros.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaginaToros.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
