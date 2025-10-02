7a702e0d0d59d53ea288833f372782fa8773ee7d
CorsaniM <maximocorsani@gmail.com>
2025-10-02 13:16:41 -0300

    hotfix

--- PaginaToros/Client/obj/Debug/net6.0/PaginaToros.Client.AssemblyInfo.cs
+++ PaginaToros/Client/obj/Debug/net6.0/PaginaToros.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaginaToros.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7542c7930bb28078656ca4ceee21ce74bfda309")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77299257bfc33108370367a5e3603926dc4fe89c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaginaToros.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaginaToros.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PaginaToros/Client/obj/Debug/net6.0/service-worker/service-worker.js
+++ PaginaToros/Client/obj/Debug/net6.0/service-worker/service-worker.js
@@ -2,4 +2,4 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
-/* Manifest version: XHTzjiLr */
+/* Manifest version: 55Cq3zgg */
