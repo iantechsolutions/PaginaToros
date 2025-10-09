93b52b2f0e66ad92c091e0e3299092f8ae1c4303
CorsaniM <maximocorsani@gmail.com>
2025-10-09 12:10:48 -0300

    hotfix

--- PaginaToros/Shared/obj/Release/net6.0/PaginaToros.Shared.AssemblyInfo.cs
+++ PaginaToros/Shared/obj/Release/net6.0/PaginaToros.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaginaToros.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a702e0d0d59d53ea288833f372782fa8773ee7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a563c5083b68fa524283098ac12b837dd515bb0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaginaToros.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaginaToros.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
