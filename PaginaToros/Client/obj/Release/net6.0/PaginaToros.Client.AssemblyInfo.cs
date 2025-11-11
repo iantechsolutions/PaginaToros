a839b96db8f4c10e458414c63bc4eba27a6a1f1f
CorsaniM <maximocorsani@gmail.com>
2025-11-11 08:52:00 -0300

    hotfix envio mails desde socios.

--- PaginaToros/Client/obj/Release/net6.0/PaginaToros.Client.AssemblyInfo.cs
+++ PaginaToros/Client/obj/Release/net6.0/PaginaToros.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaginaToros.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d55d4fed221a2ac395529c1a641406a201d75c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+668ca53702e5ad41f1671091140349037510b289")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaginaToros.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaginaToros.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
