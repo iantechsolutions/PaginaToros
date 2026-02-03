1376f3ad52fb7d9d0651ff188a973dd0676c76ae
CorsaniM <maximocorsani@gmail.com>
2026-02-03 11:45:47 -0300

    HOTFIX

--- PaginaToros/Client/obj/Release/net6.0/PaginaToros.Client.AssemblyInfo.cs
+++ PaginaToros/Client/obj/Release/net6.0/PaginaToros.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaginaToros.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03eec13ae9da28f2e1a0557bfdb299b4c359080")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b5a1507449e8c5b7c7defb2ac903f7e9c6809e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaginaToros.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaginaToros.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
